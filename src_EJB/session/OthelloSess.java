aab7f6be6e4b67fac06958caa9fee817f0f3c0ef
Ophiran <ophiran@hotmail.com>
2013-10-22 11:40:22 +0200

    initial commit

---
+++ src_EJB/session/OthelloSess.java
@@ -0,0 +1,22 @@
+package session;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class OthelloSess
+ */
+@Stateless
+@LocalBean
+public class OthelloSess implements OthelloSessRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public OthelloSess() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+
+}
